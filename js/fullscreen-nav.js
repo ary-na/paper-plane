645d7e7f18561b3eca3304e9e3267c19c52473d8
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-07-28 14:07:15 +1000

    Create fullscreen nav menu

--- js/fullscreen-nav.js
+++ js/fullscreen-nav.js
@@ -1,6 +1,6 @@
 // Code sourced and adapted from:
 // https://javascript.info/arrow-functions-basics
-//
+// https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp
 
 // Open the navigation menu by setting the width to full width.
 let openNavMenu = () => document.querySelector("#navMenu").style.width = "100%";
