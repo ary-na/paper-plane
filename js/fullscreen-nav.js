3be9c9bbff4f8660d29037b6898959ef3d91e877
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-07-28 13:40:30 +1000

    Update .gitignore

---
+++ js/fullscreen-nav.js
@@ -0,0 +1,9 @@
+// Code sourced and adapted from:
+// https://javascript.info/arrow-functions-basics
+//
+
+// Open the navigation menu by setting the width to full width.
+let openNavMenu = () => document.querySelector("#navMenu").style.width = "100%";
+
+// Close the navigation menu by setting the width to zero width.
+let closeNavMenu = () => document.querySelector("#navMenu").style.width = "0";
