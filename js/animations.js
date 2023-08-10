57dee3a26bc3157d1c682d5d0f7068e9aaa9c69c
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-08-10 22:57:14 +1000

    Minor changes and updates

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,14 @@
 // Code sourced and adapted from:
 // https://animejs.com/documentation/#motionPath
-let paperPlanePath = anime.path('#route');
+let polygonPath = anime.path('#route');
 
 // The settings for the motion path animation of the paper plane on the home screen.
 let paperPlaneMotionPath = anime({
-    targets: '#paperPlane',
-    translateX: paperPlanePath('x'),
-    translateY: paperPlanePath('y'),
-    rotate: paperPlanePath('angle'),
+    targets: '#polygon',
+    translateX: polygonPath('x'),
+    translateY: polygonPath('y'),
+    rotate: polygonPath('angle'),
     easing: 'linear',
-    duration: 20000,
+    duration: 25000,
     loop: true
 });
